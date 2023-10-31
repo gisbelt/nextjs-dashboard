c6719e1913a69ee7d17d844502b1563c440fdb00
GisbelT <gisbeltq@gmail.com>
2023-10-31 00:07:55 -0400

    Add: streaming loading.tsx

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -1,9 +1,9 @@
 import { Card } from '@/app/ui/dashboard/cards';
+import { lusitana } from '@/app/ui/fonts';
+import { fetchCardData, fetchLatestInvoices, fetchRevenue } from '../../lib/data';
 import RevenueChart from '@/app/ui/dashboard/revenue-chart';
 import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
-import { lusitana } from '@/app/ui/fonts';
-import { fetchCardData, fetchLatestInvoices, fetchRevenue } from '../lib/data';
- 
+
 export default async function Page() {
 	const revenue = await fetchRevenue();
 	const latestInvoices = await fetchLatestInvoices();
